1ba06cb4a3b43d80622a2380ac7c36b38bf7cd2f
jadtfen <119529173+jadtfen@users.noreply.github.com>
2024-06-28 18:28:53 -0400

    Update server.js
    fixed

--- server.js
+++ server.js
@@ -1,54 +1,54 @@
-// register
-app.post('/api/register', async (req, res, next) =>
-{
-    // incoming: email, name, password 
+const express = require('express');
+const app = express();
+const { MongoClient } = require('mongodb');
+const client = new MongoClient('your_mongo_db_connection_string');
+
+app.use(express.json());
+
+app.post('/api/register', async (req, res, next) => {
     var { email, name, password } = req.body;
-    var status = 0;
+    var status = 0; // Default status value
     const db = client.db('party-database');
     var error = "none";
 
-    // Check if email is valid
-    var emailRegex  = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if(!emailRegex.test(email))
-    {
+    var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (!emailRegex.test(email)) {
         email = '';
         error = "Invalid email";
     }
-    
-    // Check if email is already taken
-    var results = await db.collection('users').find({email:email}).toArray();
-    if (results.length > 0)
-    {
+
+    var results = await db.collection('users').find({ email: email }).toArray();
+    if (results.length > 0) {
         email = '';
         error = "Email already in use";
     }
 
-    // Check if password is 8-23 characters and has at least 1 number, special character, and upper case
-    var passwordRegex  = /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*]{8,32}$/;
-    if (!passwordRegex.test(password))
-    {
+    var passwordRegex = /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*]{8,32}$/;
+    if (!passwordRegex.test(password)) {
         password = '';
         error = "Weak password";
     }
-    
-    // If email, name, password valid then create new user
-    if(password != '' && email != '' && name != '')
-    {
-        try
-        {
-            //const newUser = {login:login, email:email, password:password,userID:userID};
-            const newUser = {name:name, email:email, password:password, status:status};
 
-            results = db.collection('users').insertOne(newUser);
-        }
-
-        catch(e)
-        {
+    if (password !== '' && email !== '' && name !== '') {
+        try {
+            const newUser = { name: name, email: email, password: password, status: status };
+            results = await db.collection('users').insertOne(newUser);
+        } catch (e) {
             error = e.toString();
         }
     }
 
-    // Return array: email, name, password, status, error message
-    var ret = { name:name,email:email,password:password, status:status, error:error};
+    var ret = { name: name, email: email, password: password, status: status, error: error };
     res.status(200).json(ret);
+});
+
+client.connect(err => {
+    if (err) {
+        console.error('Failed to connect to the database. Exiting now...');
+        process.exit();
+    } else {
+        app.listen(5000, () => {
+            console.log('Server is running on port 5000');
+        });
+    }
 });
