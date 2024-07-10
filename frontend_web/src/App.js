1a8cb17e8b69ce2cb0ef2c06b5c91b050ee9cade
jadtfen <119529173+jadtfen@users.noreply.github.com>
2024-07-09 20:00:10 -0400

    Update App.js

--- frontend_web/src/App.js
+++ frontend_web/src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/Landingpage';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
+import JoinPage from './pages/JoinPage';
+import CreatePartyPage from './pages/CreateaPartyPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<LandingPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/join" element={<JoinPage />} />
+        <Route path="/createParty" element={<CreatePartyPage />} />
       </Routes>
     </BrowserRouter>
   );
