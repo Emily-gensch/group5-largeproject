2a476ff24857262447c8765d1f0e2a2b6969216e
jadtfen <119529173+jadtfen@users.noreply.github.com>
2024-07-16 19:41:01 -0400

    Update LoginPage.js

--- frontend_web/src/pages/LoginPage.js
+++ frontend_web/src/pages/LoginPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './styles/Login.css';
+import './styles/LoginPage.css';
 
 function LoginPage() {
   const [loginEmail, setLoginEmail] = useState('');
