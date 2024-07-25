b6eae705a13573e55d6a73bcda4f5345f0a4a9cf
Josaiah Saldana <josaiahsal@gmail.com>
2024-07-25 04:59:00 -0400

    Updated CORS to include the frontend url

---
+++ server.js
@@ -0,0 +1,196 @@
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+
+const app = express();
+
+// MongoDB connection
+const url = process.env.MONGO_URI_PARTY;
+
+mongoose.set('strictQuery', true);
+
+if (process.env.NODE_ENV !== 'test') {
+  console.log('MongoDB URI:', url);
+  mongoose
+    .connect(url, {
+      dbName: 'party-database',
+    })
+    .then(() => console.log('MongoDB connected'))
+    .catch((err) => console.log(err));
+}
+
+// Mongoose Models
+const User = require('./Project-API/src/models/User');
+const Party = require('./Project-API/src/models/Party');
+const Poll = require('./Project-API/src/models/Poll');
+const PartyGuest = require('./Project-API/src/models/PartyMembers');
+const Movie = require('./Project-API/src/models/Movie');
+
+app.use(
+  cors({
+    origin: 'http://localhost:5002',
+    credentials: true,
+  })
+);
+app.use(bodyParser.json());
+app.use(express.json());
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: url,
+      dbName: 'party-database',
+      collectionName: 'sessions',
+    }),
+    cookie: {
+      maxAge: 1000 * 60 * 60,
+      sameSite: 'Lax',
+      secure: false,
+    },
+  })
+);
+
+// Routes
+const authRouter = require('./Project-API/src/routes/auth');
+const partyRouter = require('./Project-API/src/routes/party');
+const pollRouter = require('./Project-API/src/routes/poll');
+
+app.use('/api/auth', authRouter);
+app.use('/api/party', partyRouter);
+app.use('/api/poll', pollRouter);
+
+app.get('/', (req, res) => {
+  if (!req.session.views) {
+    req.session.views = 0;
+  }
+  req.session.views++;
+  res.send(`Number of views: ${req.session.views}`);
+});
+
+app.get('/api/check-session', (req, res) => {
+  res.json(req.session);
+});
+
+app.post('/api/displayMovies', async (req, res) => {
+  try {
+    const movies = await Movie.find({}).sort({ title: 1 }).exec();
+    res.status(200).json(movies);
+  } catch (e) {
+    res.status(500).json({ error: e.toString() });
+  }
+});
+
+// Display watched movies
+app.post('/api/displayWatchedMovies', async (req, res) => {
+  const { partyID } = req.body;
+  try {
+    const polls = await Poll.find({ partyID, watchedStatus: 1 });
+    const movieIDs = polls.flatMap((poll) =>
+      poll.movies.map((movie) => movie.movieID)
+    );
+    const movies = await Movie.find({ _id: { $in: movieIDs } });
+    res.status(200).json(movies);
+  } catch (e) {
+    res.status(500).json({ error: e.toString() });
+  }
+});
+
+app.get('/getPartyMembers', async (req, res) => {
+  const userID = req.session.userId;
+
+  if (!userID) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  try {
+    const partyMember = await PartyMembers.findOne({ userID }).populate(
+      'partyID'
+    );
+
+    if (!partyMember) {
+      return res.status(404).json({ message: 'Party not found' });
+    }
+
+    const members = await PartyMembers.find({
+      partyID: partyMember.partyID._id,
+    }).populate('userID', 'username email');
+
+    res.status(200).json({ members });
+  } catch (error) {
+    console.error('Error fetching party members:', error);
+    res.status(500).json({ message: 'Server error', error: error.message });
+  }
+});
+
+// Search movies
+app.post('/api/searchMovie', async (req, res) => {
+  const { search } = req.body;
+  try {
+    const movies = await Movie.find({ title: new RegExp(search, 'i') });
+    res.status(200).json(movies.map((movie) => movie.title));
+  } catch (e) {
+    res.status(500).json({ error: e.toString() });
+  }
+});
+
+// Display user account
+app.post('/api/userAccount', async (req, res) => {
+  const { userID } = req.body;
+  try {
+    const user = await User.findById(userID);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.status(200).json(user);
+  } catch (e) {
+    res.status(500).json({ error: e.toString() });
+  }
+});
+
+// Change password
+app.post('/api/changePassword', async (req, res) => {
+  const { userID, newPassword, validatePassword } = req.body;
+  const passwordRegex =
+    /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*]{8,32}$/;
+
+  if (newPassword !== validatePassword) {
+    return res.status(400).json({ error: 'Passwords must match' });
+  }
+  if (!passwordRegex.test(newPassword)) {
+    return res.status(400).json({ error: 'Weak password' });
+  }
+
+  try {
+    const user = await User.findById(userID);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    const isSamePassword = bcrypt.compareSync(newPassword, user.password);
+    if (isSamePassword) {
+      return res
+        .status(400)
+        .json({ error: 'Password matches current password' });
+    }
+    user.password = bcrypt.hashSync(newPassword, 8);
+    await user.save();
+    res.status(200).json({ message: 'Password changed successfully' });
+  } catch (e) {
+    res.status(500).json({ error: e.toString() });
+  }
+});
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
